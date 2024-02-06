9b34e35d502288785e7b8b21d5959ddd050aacc6
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-02-06 16:04:13 +0530

    content update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-// Import necessary modules
 import express from 'express';
 import * as dotenv from 'dotenv';
 import UserRoute from "./routes/userRoute";
@@ -7,12 +6,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Add middleware to parse JSON in the request body
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello, TypeScript with Express!');
-});
 
 // user
 app.use('/api/user', UserRoute);
