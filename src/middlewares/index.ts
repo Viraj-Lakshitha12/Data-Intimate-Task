9b91b1c756812744f4e43ba8dc4f4e685bfa500b
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-02-07 09:07:33 +0530

    verify toekn update

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,4 +1,4 @@
-import jwt, {Secret} from "jsonwebtoken";
+import jwt, { Secret } from "jsonwebtoken";
 import express from "express";
 
 // Token verify
@@ -6,7 +6,7 @@ export const verifyToken = (req: express.Request, res: any, next: express.NextFu
     const token = req.get("authorization");
 
     if (!token) {
-        return res.status(401).json({error: 'Missing token', message: 'Authorization token is required'});
+        return res.status(401).json({ error: 'Missing token', message: 'Authorization token is required' });
     }
 
     try {
@@ -14,14 +14,11 @@ export const verifyToken = (req: express.Request, res: any, next: express.NextFu
         res.tokenData = data;
         next();
     } catch (error) {
-        console.error('Token Verification Error:', error);
         if (error instanceof jwt.TokenExpiredError) {
-            return res.status(401).json({error: 'Token expired', message: 'Authorization token has expired'});
+            return res.status(401).json({ error: 'Token expired', message: 'Authorization token has expired' });
         } else if (error instanceof jwt.JsonWebTokenError) {
-            return res.status(401).json({error: 'Invalid token', message: 'Authorization token is invalid'});
+            return res.status(401).json({ error: 'Invalid token', message: 'Authorization token is invalid' });
         } else {
-            // Handle other errors or log them as needed
-            console.error('Unhandled token verification error:', error);
             return res.status(401).json({
                 error: 'Token verification failed',
                 message: 'Failed to verify the authorization token'
