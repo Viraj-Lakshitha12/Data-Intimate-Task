14f3bce37ff7153f2d224567326a93c30ad9f361
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2024-02-08 09:13:16 +0530

    content update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import bodyParser from "body-parser";
+import cors from "cors";
 import * as dotenv from 'dotenv';
 import UserRoute from "./routes/userRoute";
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
